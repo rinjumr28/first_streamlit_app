b25fa4f1bde41406e43c5921dc66c242fcfc8d8e
rinjumr28 <132167932+rinjumr28@users.noreply.github.com>
2023-04-29 15:36:06 -0500

    Update streamlit_app.py
    Adding contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Healthy breakfast Menu')
+streamlit.text('Scrambled Egg')
+streamlit.text('Oatmeal - Blueberry')
+streamlit.text('Smootie - Kale and Spinach')
