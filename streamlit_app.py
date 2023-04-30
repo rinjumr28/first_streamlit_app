adc8453304bd84cb20a8c0f59d8ebb927b772425
rinjumr28 <132167932+rinjumr28@users.noreply.github.com>
2023-04-29 19:40:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 def get_fruit_load_list():
     with my_cnx.cursor as my_cur:
+        my_cur = my_cnx.cursor()
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
     
